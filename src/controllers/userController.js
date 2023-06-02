bfd8566e1e7a16496b1b9f2b681b075763cc87e4
Thanh Bui <ltvphuoctien@gmail.com>
2023-06-02 09:40:15 +0700

    feat:#38

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -22,7 +22,7 @@ let handleLogin = async (req, res) => {
 };
 
 let handleGetAllUsers = async (req, res) => {
-  let id = req.body.type;
+  let id = req.query.id;
   if(!id){
     return res.status(200).json({
       errCode: 1,
