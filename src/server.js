08e3fcfb990dad5f22fa14fa31bff2e5f120e546
Thanh Bui <ltvphuoctien@gmail.com>
2023-06-09 15:47:08 +0700

    feat:#47

--- src/server.js
+++ src/server.js
@@ -3,37 +3,41 @@ import bodyParser from "body-parser"; //giúp ra lấy được các tham số m
 import configViewEngine from "./config/viewEngine";
 import initWebRoutes from "./route/web";
 import connectDB from "./config/connectDB";
-// import cors from "cors"
 require("dotenv").config();
 
 let app = express();
-// app.use(cors({ credentials: true, origin: true }));
+
+
 
 // Add headers before the routes are defined
 app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
 
-    // Website you wish to allow to connect
-    res.setHeader(
-      "Access-Control-Allow-Origin",
-      process.env.REACT_APP_BACKEND_URL
-    );
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
 
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
 
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", true);
 
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
+  // Pass to next layer of middleware
+  next();
 });
 
-//config app
 
+
+
+//config app
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -41,8 +45,15 @@ configViewEngine(app);
 initWebRoutes(app);
 connectDB();
 
+
+
+
 let port = process.env.PORT || 6969;
 //Port === undefined => port = 6969
+
+
+
+
 
 app.listen(port, () => {
   //callback
