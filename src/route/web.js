3e0aace4a5bd65f2ac0c945b814ad8018d05f770
Thanh Bui <ltvphuoctien@gmail.com>
2023-07-07 20:44:20 +0700

    feat: add one new api

--- src/route/web.js
+++ src/route/web.js
@@ -19,6 +19,7 @@ let initWebRoutes = (app) => {
   router.post("/api/create-new-user", userController.handleCreateNewUser);
   router.put("/api/edit-user", userController.handleEditUser);
   router.delete("/api/delete-user", userController.handleDeleteUser);
+  router.get("/api/allcode", userController.getAllCode);
   router.get("/allcode", userController.getAllCode);
 
   return app.use("/", router);
